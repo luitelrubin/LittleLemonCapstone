1f3570e8ad49ebd9b1c6d271ec58b057ee97867a
Rubin <rubinprasadluitel@gmail.com>
2025-08-15 12:38:32 +0545

    Project: Make MySQL the default databse and remove db.sqlite3

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -77,11 +77,11 @@ WSGI_APPLICATION = "littlelemon.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # },
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    },
-    "mydb": {
         "ENGINE": "django.db.backends.mysql",
         "NAME": config("DB_NAME"),
         "USER": config("USER_NAME"),
